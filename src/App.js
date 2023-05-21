2db854af8c1450cb210aa7356894012001a85216
yhd1101 <asd159369@naver.com>
2023-05-21 13:36:24 +0900

    react_fun,state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  //함수, 상수, 상태 등을 선언하는곳 return은 화면에 보여주는곳
+    const [keyword, setKeyword] = useState("")
+
+    // 키워드를 변경하는 함수
+    const changeKeyword = () => {
+        console.log("키워드임")
+        setKeyword("키워드")
+    }
+
+    return (
+        <div>
+          <h1>{keyword}</h1>
+            <button onClick={changeKeyword}>키워드 변경</button>
+        </div>
+    );
+};
 
 export default App;
