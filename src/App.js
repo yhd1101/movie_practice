f13245874fa79466a8984384fda1be665aabe05a
yhd1101 <asd159369@naver.com>
2023-05-21 16:42:56 +0900

    Detail

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
-import React, {useState} from 'react';
-import axios from "axios";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import {Grid, Container} from "@mui/material";
+import React from 'react';
+import Main from "./pages/Main";
+import {RouterProvider} from "react-router-dom";
+import router from "./router";
 
 const App = () => {
-    const [news, setNews] = useState([])
-
-    const getNews =async () =>{
-        try{
-            const result = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2023-04-21&sortBy=publishedAt&apiKey=f1c87e2bb86248de9f9492e513f93e1f")
-            console.log(result.data.articles)
-            setNews(result.data.articles)
-        } catch (err){
-            console.log(err)
-        }
-    }
-    //자동실행 함수
-    useState(() => {
-        getNews()
-    }, [])
-
-
     return (
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-           {news && news.map(n => (
-               <Card sx={{ maxWidth: 345 }}>
-                   <CardMedia
-                       sx={{ height: 120 }}
-                       image={n.urlToImage}
-                       title="green iguana"
-                   />
-                   <CardContent>
-                       <Typography gutterBottom variant="h5" component="div">
-                           {n.title.slice(0,13)}
-                       </Typography>
-                       <Typography variant="body2" color="text.secondary">
-                           {n.description.slice(0,150)}
-                       </Typography>
-                   </CardContent>
-                   <CardActions>
-                       <Button size="small">Share</Button>
-                       <Button size="small">자세히 보기</Button>
-                   </CardActions>
-               </Card>
-           ))}
-        </Grid>
+        <RouterProvider router={router} />
     );
 };
 
---
+++ src/pages/Tv.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tv = () => {
+    return (
+        <div>
+            <h1>Tv</h1>
+        </div>
+    );
+};
+
+export default Tv;
